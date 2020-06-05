35e78a0ec5b88a8178e8e6938b41350441ec5bc8
marcelodosanjosc <marcelodosanjosc@gmail.com>
2020-06-05 19:40:29 -0400

    Controle de cadastro de funcionario um sistema feito junto com a fundação bradesco

---
+++ src/sistema/BancoDeDados.java
@@ -0,0 +1,7 @@
+package sistema;
+
+public class BancoDeDados {
+    public static String stringDeConexao = "jdbc:postgresql://localhost:5432/sistema_de_funcionarios";
+    public static String usuario = "postgres";
+    public static String senha = "admin";
+}
